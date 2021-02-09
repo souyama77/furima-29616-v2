e7337f6b97fa7807d743ad0b4a3646551b21fb47
souyama77 <p14p083@yahoo.co.jp>
2021-02-09 09:27:27 +0900

    商品出品機能

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+function calculation() {
+  const itemPrice = document.getElementById("item-price")
+  const taxPrice = document.getElementById("add-tax-price")
+  const gain = document.getElementById("profit")
+
+  itemPrice.addEventListener("input",function () {
+    const price = itemPrice.value;
+    const tax = price * 0.1
+    const profit = price * 0.9;
+    taxPrice.innerHTML = tax
+    gain.innerHTML = profit
+  })
+ }
+  window.addEventListener('load', calculation);
---
+++ app/models/item.rb
@@ -0,0 +1,29 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture_code
+  belongs_to_active_hash :preparation_day
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :category
+    validates :item_condition
+    validates :postage_payer
+    validates :prefecture_code
+    validates :preparation_day
+    validates :price, inclusion: {in: 300...9_999_999}, format: {with:/\A[0-9]+\z/}
+  end
+  with_options numericality:{ other_than:0} do
+    validates :category_id
+    validates :item_condition_id
+    validates :postage_payer_id
+    validates :prefecture_code_id
+    validates :preparation_day_id
+  end
+  belongs_to :user
+  has_one_attached :image
+end
